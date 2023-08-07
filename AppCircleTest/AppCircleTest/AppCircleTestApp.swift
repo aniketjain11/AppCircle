bc2c27c70e907953bf72850ac68d6e3963714e71
Aniket Jain <aniketj@dewsolutions.in>
2023-08-07 14:57:07 +0530

    initial commit

---
+++ AppCircleTest/AppCircleTest/AppCircleTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppCircleTestApp.swift
+//  AppCircleTest
+//
+//  Created by Aniket Jain on 07/08/23.
+//
+
+import SwiftUI
+
+@main
+struct AppCircleTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
